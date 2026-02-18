8bba904f336d242e068a395b5de0defeaebbdc50
sukebenedek <suke.benedek@students.jedlik.eu>
2026-02-18 20:18:24 +0100

    init

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            UserAppTheme = AppTheme.Dark;
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiApp1/Pages/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
